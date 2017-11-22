fc302e352c11206dd551f080c61b7ad28e462075
Matthew Gates <matthewg42@gmail.com>
2017-11-22 17:23:03 +0000

    add FadeChase

---
+++ src/firmware/FadeChase.cpp
@@ -0,0 +1,43 @@
+#include <Millis.h>
+#include <MutilaDebug.h>
+#include "FadeChase.h"
+
+FadeChase::FadeChase(CRGB* ledData, uint16_t numLeds, CRGB* colors, uint16_t numColors, uint16_t shiftPeriodMs, uint16_t colorPeriodMs) :
+    StripEffect(ledData, numLeds),
+    _colors(colors), 
+    _numColors(numColors),
+    _shiftPeriodMs(shiftPeriodMs),
+    _colorPeriodMs(colorPeriodMs),
+    _lastShift(0),
+    _lastColorChange(0),
+    _colorIndex(0)
+{
+}
+
+void FadeChase::render()
+{
+    if (Millis() > _lastColorChange + _colorPeriodMs) {
+        _lastColorChange = Millis();
+        _colorIndex = (_colorIndex + 1) % _numColors;
+        DB(F("FadeChase::render color="));
+        DBLN(_colorIndex);
+    }
+
+    if (Millis() > _lastShift + _shiftPeriodMs) {
+        _lastShift = Millis();
+
+        // Shift all colors to the right
+        for(uint16_t i=_numLeds-1; i>0; i--) {
+            _ledData[i] = _ledData[i-1];
+        }
+
+        uint16_t nextColorIndex = (_colorIndex + 1) % _numColors;
+        uint16_t msSinceLastChange = Millis() - _lastColorChange;
+        float fade = (float)msSinceLastChange / _colorPeriodMs;
+        if (fade > 1) fade = 1;
+
+        // Put the current color at the start
+        _ledData[0] = _colors[_colorIndex].lerp16(_colors[nextColorIndex], 65535*fade);
+    }
+}
+
