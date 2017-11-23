127b188f3f89d26c134011b7493dc3b849a91b01
Matthew Gates <matthewg42@gmail.com>
2017-11-23 14:13:28 +0000

    120 seems to be about out limit

--- src/firmware/Config.h
+++ src/firmware/Config.h
@@ -4,8 +4,7 @@
 #include <FastLED.h>
 
 #define             LedChipset              WS2812B
-const uint16_t      LedCount =              150;
-//const uint16_t      LedCount =              48;
+const uint16_t      LedCount =              120;
 const EOrder        LedOrder =              GRB;
 const uint8_t       LedPin =                4;
 const uint16_t      LedRefreshMs =          10;
