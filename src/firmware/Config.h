4bf81ac1ea91f26ec8af498f4d0ed7b2a191de63
Matthew Gates <matthewg42@gmail.com>
2017-11-23 01:42:46 +0000

    memory test (150 LEDs, fade)

--- src/firmware/Config.h
+++ src/firmware/Config.h
@@ -4,8 +4,8 @@
 #include <FastLED.h>
 
 #define             LedChipset              WS2812B
-//const uint16_t      LedCount =              150;
-const uint16_t      LedCount =              48;
+const uint16_t      LedCount =              150;
+//const uint16_t      LedCount =              48;
 const EOrder        LedOrder =              GRB;
 const uint8_t       LedPin =                4;
 const uint16_t      LedRefreshMs =          10;
@@ -17,3 +17,4 @@ const bool          ButtonPullup =          true;
 const uint8_t       HeartbeatPin =          13;
 
 const uint8_t       BrightnessFaderPin =    A7;
+const uint8_t       SpeedControlPin =       A6;
---
+++ src/firmware/SpeedControl.cpp
@@ -0,0 +1,5 @@
+#include "SpeedControl.h"
+#include "Config.h"
+
+DiscretePot SpeedControl(SpeedControlPin);
+
