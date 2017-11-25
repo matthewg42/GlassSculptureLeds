ca76c4fbd037db821fe3643550a720ab2cf0c142
Matthew Gates <matthewg42@gmail.com>
2017-11-25 00:49:18 +0000

    LedCount can be set from -Defines

--- src/firmware/Config.h
+++ src/firmware/Config.h
@@ -4,12 +4,17 @@
 #include <FastLED.h>
 
 #define             LedChipset              WS2812B
-const uint16_t      LedCount =              120;
+
+#ifdef LEDCOUNT
+const uint16_t      LedCount =              LEDCOUNT;
+#else
+const uint16_t      LedCount =              200;
+#endif
 const EOrder        LedOrder =              GRB;
 const uint8_t       LedPin =                4;
 const uint16_t      LedRefreshMs =          10;
 
-const uint16_t      TransitionMs =          500;
+const uint16_t      TransitionMs =          1500;
 
 // The mode button
 const uint8_t       ButtonPin =             3;
